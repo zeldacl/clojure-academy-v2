3ac0436de57b05cf6bdd0d1e855d9724c353f310
zeldacl <zeldacl@gmail.com>
2026-03-18 00:30:20 +0800

    temp save

--- minecraftmod/ac/src/main/clojure/my_mod/item/constraint_plate.clj
+++ minecraftmod/ac/src/main/clojure/my_mod/item/constraint_plate.clj
@@ -3,7 +3,8 @@
   
   Required for Matrix to function (need 3 plates)."
   (:require [my-mod.item.dsl :as idsl]
-            [my-mod.util.log :as log]))
+            [my-mod.util.log :as log]
+            [clojure.string :as str]))
 
 ;; ============================================================================
 ;; Constraint Plate Item
@@ -23,7 +24,28 @@
   (log/info "Constraint Plate initialized"))
 
 (defn is-constraint-plate?
-  "Check if ItemStack is a constraint plate"
+  "Check if ItemStack (or item-like value) is a constraint plate.
+
+  Accepts either a platform `ItemStack` or an item/spec-like map.
+  For ItemStacks we extract the description id and compare against the
+  DSL `:id` to make the predicate robust across representations.
+  "
   [item-stack]
   (when item-stack
-    (= (.getItem item-stack) constraint-plate)))
+    (let [id-from-spec #(when (map? %) (:id %))
+          desc (try
+                 (let [item (.getItem item-stack)]
+                   (str (.getDescriptionId item)))
+                 (catch Throwable _ nil))
+          id-from-stack (when desc (last (str/split desc #"\\.")))
+          id (or id-from-stack (id-from-spec item-stack))
+          result (boolean
+                  (or (= id (:id constraint-plate))
+                      (and (string? desc) (str/includes? desc (:id constraint-plate)))))]
+      (try
+        (log/debug "is-constraint-plate?" {:stack-class (class item-stack)
+                                            :desc desc
+                                            :derived-id id
+                                            :result result})
+        (catch Throwable _))
+      result)))
