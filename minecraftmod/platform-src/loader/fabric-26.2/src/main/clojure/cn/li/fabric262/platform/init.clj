8db6204107105299af400fed99f4c48cf12a4286
zeldacl <zeldacl@gmail.com>
2026-08-07 23:23:18 +0800

    fix: defer Fabric block entity class resolution

--- minecraftmod/platform-src/loader/fabric-26.2/src/main/clojure/cn/li/fabric262/platform/init.clj
+++ minecraftmod/platform-src/loader/fabric-26.2/src/main/clojure/cn/li/fabric262/platform/init.clj
@@ -13,10 +13,15 @@
             [cn.li.mcbase.platform.menu-inventory-ops :as menu-inventory-ops]
             [cn.li.mcmod.runtime.install :as install]
             [cn.li.platform.adapter.minecraft-ops :as minecraft-ops])
-  (:import [cn.li.fabric262.block.entity ScriptedBlockEntity]
-           [net.minecraft.core BlockPos]
+  (:import [net.minecraft.core BlockPos]
            [net.minecraft.world.level Level]
            [net.minecraft.world.level.block.state BlockState]))
+
+(defn- scripted-be-class
+  []
+  ;; Resolve after Minecraft bootstrap; Class/forName is intentionally kept
+  ;; out of the namespace import table so AOT does not initialize BlockEntity.
+  (Class/forName "cn.li.fabric262.block.entity.ScriptedBlockEntity"))
 
 (defn- resolve-binding! [binding-name]
   (require 'cn.li.fabric262.platform.bindings)
@@ -26,14 +31,14 @@
 
 (defn- install-be-ops! []
   (core/install-be-fns!
-    {:be-get-level (fn [^ScriptedBlockEntity be] (.getLevel be))
-     :be-get-world (fn [^ScriptedBlockEntity be] (.getLevel be))
-     :be-get-custom-state (fn [^ScriptedBlockEntity be] (.getCustomState be))
-     :be-set-custom-state! (fn [^ScriptedBlockEntity be state] (.setCustomState be state))
-     :be-get-block-id (fn [^ScriptedBlockEntity be] (.getBlockId be))
-     :be-set-changed! (fn [^ScriptedBlockEntity be] (.setChanged be))
-     :be-sync-to-client! (fn [^ScriptedBlockEntity be] (.syncCustomStateToClient be))
-     :be-get-fluid-height (fn [^ScriptedBlockEntity be]
+    {:be-get-level (fn [be] (.getLevel be))
+     :be-get-world (fn [be] (.getLevel be))
+     :be-get-custom-state (fn [be] (.getCustomState be))
+     :be-set-custom-state! (fn [be state] (.setCustomState be state))
+     :be-get-block-id (fn [be] (.getBlockId be))
+     :be-set-changed! (fn [be] (.setChanged be))
+     :be-sync-to-client! (fn [be] (.syncCustomStateToClient be))
+     :be-get-fluid-height (fn [be]
                             (try
                               (when-let [^Level level (.getLevel be)]
                                 (let [^BlockPos pos (.getBlockPos be)
@@ -48,7 +53,7 @@
   (minecraft-ops/build-adapter-map
    (merge (runtime-ops/standard-runtime-ops)
           {:local-player-class (fn [] nil)
-           :scripted-be-class (fn [] ScriptedBlockEntity)
+           :scripted-be-class scripted-be-class
            :world-place-block-by-id
            (fn [level block-id pos flags]
              ((resolve-binding! 'world-place-block-by-id) level block-id pos flags))})))
