4dce575fc10bf8d27ceda3170b74899a6890c504
zeldacl <zeldacl@gmail.com>
2026-08-16 23:48:03 +0800

    add Combat Core metadata catalog boundary

---
+++ minecraftmod/ac/src/test/clojure/cn/li/ac/ability/registry/catalog_merge_test.clj
@@ -0,0 +1,11 @@
+(ns cn.li.ac.ability.registry.catalog-merge-test
+  (:require [clojure.test :refer [deftest is]]
+            [cn.li.ac.ability.registry.catalog-merge :as merge]))
+
+(deftest combat-metadata-wins-duplicate-ids
+  (is (= [{:id :arc-gen :execution :combat-core}
+          {:id :brain-course :execution :legacy}]
+         (merge/merge-skill-specs
+          [{:id :arc-gen :execution :combat-core}]
+          [{:id :arc-gen :execution :legacy}
+           {:id :brain-course :execution :legacy}]))))
---
+++ minecraftmod/ac/src/test/clojure/cn/li/ac/ability/registry/combat_catalog_test.clj
@@ -0,0 +1,20 @@
+(ns cn.li.ac.ability.registry.combat-catalog-test
+  (:require [clojure.test :refer [deftest is use-fixtures]]
+            [cn.li.ac.ability.registry.combat-catalog :as catalog]))
+
+(use-fixtures :each
+  (fn [f]
+    (catalog/reset-for-test!)
+    (f)
+    (catalog/reset-for-test!)))
+
+(deftest metadata-registry-does-not-require-legacy-actions
+  (is (true? (catalog/register-skills!
+              [{:id :arc-gen :category-id :electromaster :level 1
+                :controllable? true :ctrl-id :arc-gen :pattern :instant
+                :execution :combat-core
+                :combat-ability-id :arc-gen}])))
+  (is (= :combat-core (:execution (catalog/get-skill :arc-gen))))
+  (is (catalog/installed?))
+  (is (= [:arc-gen]
+         (mapv first (catalog/raw-skill-entries)))))
