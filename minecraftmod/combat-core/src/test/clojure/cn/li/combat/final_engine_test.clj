2282ae388eff5f4d3e233c6811120be5e269110d
zeldacl <zeldacl@gmail.com>
2026-08-27 22:21:06 +0800

    test: cover final phase transition signal

--- minecraftmod/combat-core/src/test/clojure/cn/li/combat/final_engine_test.clj
+++ minecraftmod/combat-core/src/test/clojure/cn/li/combat/final_engine_test.clj
@@ -9,6 +9,16 @@
              {:component :flow/sequence
               :steps [{:component :combat/damage :args {:amount 1}}
                       {:component :target/raycast :bind :hit}]}))))
+(deftest flow-finish-next-phase-is-propagated-test
+  (let [runtime (engine/create-engine {:host (host/create {:queries {} :actions {}})
+                                       :state-provider (fn [_] {})
+                                       :commit-state! (fn [_])})
+        result (engine/execute! runtime (compiler/compile-program
+                                 {:component :flow/finish :outcome :anti-afk :next-phase :release})
+                                {:owner :alice :world "world:test" :ability-id :skill/a
+                                 :tick 1 :seed 9 :input {}})]
+    (is (= :accepted (:status result)))
+    (is (= :release (:next-phase result)))))
 (deftest engine-preflight-before-state-commit-test
   (let [commits (atom []) host (host/create {:queries {} :actions {:combat/damage (fn [_ _ _] true)}}) engine (engine/create-engine {:host host :state-provider (fn [_] {:resources {:mana 3}}) :commit-state! #(swap! commits conj %)}) compiled (compiler/compile-program {:component :flow/sequence :steps [{:component :resource/try-spend :resource :mana :amount 1 :bind :ok} {:component :combat/damage :args {:amount 2}}]}) result (engine/execute! engine compiled {:owner :alice :world "w" :tick 1 :seed 9 :input {}})]
     (is (= :accepted (:status result)))
