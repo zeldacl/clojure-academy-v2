0ab16fcaae023ee3a5b4de6296c41d9ccad5188b
zeldacl <zeldacl@gmail.com>
2026-09-01 01:07:11 +0800

    test: cover source-first init and IC2 isolation

--- minecraftmod/platform-src/loader/neoforge-26.2/src/test/clojure/cn/li/neoforge262/init_smoke_test.clj
+++ minecraftmod/platform-src/loader/neoforge-26.2/src/test/clojure/cn/li/neoforge262/init_smoke_test.clj
@@ -1,16 +1,17 @@
 (ns cn.li.neoforge262.init-smoke-test
   "Smoke tests for NeoForge 26.2 initialization entry points.
-  AOT class presence avoids brittle full-bootstrap; var checks cover the public API."
+  Named/source-first targets load the entry points from Clojure resources;
+  var checks cover the public API."
   (:require [clojure.test :refer [deftest is testing]]
             [clojure.java.io :as io]
             [cn.li.neoforge262.init :as init]))
 
 (deftest loader-sources-present
-  (testing "mod and platform init sources are AOT'd into the test classpath"
-    (let [mod-init (io/resource "cn/li/neoforge262/mod__init.class")
-          platform-init (io/resource "cn/li/neoforge262/platform/init__init.class")]
-      (is (some? mod-init) "cn.li.neoforge262.mod should be AOT'd into test runtime classpath")
-      (is (some? platform-init) "cn.li.neoforge262.platform.init should be AOT'd into test runtime classpath"))))
+  (testing "mod and platform init sources are available on the test classpath"
+    (let [mod-init (io/resource "cn/li/neoforge262/mod.clj")
+          platform-init (io/resource "cn/li/neoforge262/platform/init.clj")]
+      (is (some? mod-init) "cn.li.neoforge262.mod should be source-backed")
+      (is (some? platform-init) "cn.li.neoforge262.platform.init should be source-backed"))))
 
 (deftest initialization-functions-exist
   (testing "version-setting function exists"
